3b04e6fcdee78f08b361dfb8c2640babed89294d
madeleineotway <motway@cmri.org.au>
2019-03-25 15:34:15 +1100

    MJO ggplot theme added

--- R/Matrix.R
+++ R/Matrix.R
@@ -85,7 +85,7 @@ Make_matrix_plot <- function(Mat_data,Set_size_data, Main_bar_data, point_size, 
   }
 
   Matrix_plot <- (ggplot()
-                  + theme(panel.background = element_rect(fill = "white"),
+                  + theme(panel.background = element_rect(fill = "ghostwhite"),
                           plot.margin=unit(c(-0.2,0.5,0.5,0.5), "lines"),
                           axis.text.x = element_blank(),
                           axis.ticks.x = element_blank(),
